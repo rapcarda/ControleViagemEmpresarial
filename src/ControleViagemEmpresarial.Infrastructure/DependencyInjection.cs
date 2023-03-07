278317ec89acd581d01831eeac6d3c04e769ad55
Rafael Carda <rapcarda@gmail.com>
2023-03-07 17:28:19 -0300

    Criar estrutura do projeto

---
+++ src/ControleViagemEmpresarial.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ControleViagemEmpresarial.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrasctructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ src/ControleViagemEmpresarial.WebApi/Configuration/ConfigureDependencies.cs
@@ -0,0 +1,17 @@
+﻿using ControleViagemEmpresarial.Application;
+using ControleViagemEmpresarial.Infrastructure;
+using ControleViagemEmpresarial.Presentation;
+
+namespace ControleViagemEmpresarial.WebApi.Configuration
+{
+    public static class ConfigureDependencies
+    {
+        public static IServiceCollection AddProjectsDependencyInjection(this IServiceCollection services)
+        {
+            return services
+                .AddApplication()
+                .AddInfrasctructure()
+                .AddPresentation();
+        }
+    }
+}
